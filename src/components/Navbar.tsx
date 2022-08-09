7f7e6c43d921e2915352a5744e772e046ba8079a
orgil0588 <orgil0588@gmail.com>
2022-08-09 16:37:24 +0800

    changing classname

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <nav className="py-4 px-4 md:px-0 2xl:max-w-[1440px] bg-transparent backdrop-blur-lg border-b border-[#303030] fixed w-full">
-      <div className="container mx-auto">Orgil.</div>
+    <nav className="py-4 px-4 md:px-0  bg-transparent backdrop-blur-lg border-b border-[#303030] fixed w-full">
+      <div className="container mx-auto 2xl:max-w-[1440px]">Orgil.</div>
     </nav>
   );
 };
